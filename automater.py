1bebc211f5fdc11a745cbfb609c415c096f54a01
doerrelisha <33271259+doerrelisha@users.noreply.github.com>
2023-05-15 18:15:11 -0400

    modified:   .env 	modified:   automater.py 	new file:   tournamentautomater.py

--- automater.py
+++ automater.py
@@ -7,6 +7,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 # ask for types of games
 prefix = input('What is the game server?\n')
@@ -15,7 +17,8 @@ basenumber = input("What game number are you starting from?")
 game_total = input("How many games would you like?")
 
 # initiate webdriver
-driver = uc.Chrome(use_subprocess=True)
+# put this in parentheses if error happens ChromeDriverManager().install()
+driver = uc.Chrome()
 
 
 # Using Chrome to access web
@@ -59,7 +62,7 @@ for i in range(int(game_total)):
     driver.find_element(By.XPATH, "//input[@name='name']").clear()
     driver.find_element(By.XPATH, "//input[@name='name']").send_keys(prefix + " " + type + " " + game)
     if prefix == "Olympus":
-        driver.find_element(By.ID, "game_description").send_keys("For experienced and dedicated players. You must have 96% of your turns made and at least 10 games played, or you could be a part of the Olympus Diplomacy Community. https://discord.gg/9SymATUwwW")
+        driver.find_element(By.ID, "game_description").send_keys("For experienced and dedicated players. You must have 96% of your turns made and at least 10 games played, or you could be a part of the Olympus Diplomacy Community. https://discord.gg/TkYsTs6nJN")
     elif prefix == "Demosthenes" and type == "Gunboat":
         driver.find_element(By.ID, "game_description").send_keys("Demosthenes Gunboat Series. No press, fast adjudication, 24 hour game, 12 hr build/retreat. Welcome to the world of the orator! For more games, join here! https://discord.gg/4TvjxR3CuH")
     elif prefix == "Demosthenes" and type == "Press":
@@ -68,7 +71,7 @@ for i in range(int(game_total)):
         driver.find_element(By.ID, "game_description").send_keys("ERROR")
     
     driver.find_element(By.ID, "type_public").click()
-    if type == "Gunboat":
+    if type in ("Gunboat" , "Long Gunboat"):
         driver.find_element(By.ID, "press_never_allowed").click()
     else:
         driver.find_element(By.ID, "press_always_allowed").click()
@@ -79,6 +82,15 @@ for i in range(int(game_total)):
     initial = Select(driver.find_element(By.ID, 'initial_adjudication_period'))
     initial.select_by_value('2880')
 
+    springdaily = Select(driver.find_element(By.ID, 'orders_adjudication_period'))
+    if type == "Long Gunboat":
+        springdaily.select_by_value('4320')
+
+    buildretreats = Select(driver.find_element(By.ID, 'build_and_retreat_adjudication_divisor'))
+    if type == "Long Gunboat":
+        buildretreats.select_by_value('1')
+    
+
     driver.find_element(By.XPATH, "//input[@name='name']").click()
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
 
@@ -88,17 +100,25 @@ for i in range(int(game_total)):
         body.send_keys(Keys.PAGE_DOWN)
         elem = driver.switch_to.active_element
         driver.find_element(By.ID, "fast_adjudication").click()
-    elif type == "Gunboat":
+    elif type in ("Gunboat" , "Long Gunboat"):
         driver.find_element(By.ID, "begin_when_full").click()
         driver.find_element(By.ID, "skip").click()
         elem = driver.switch_to.active_element
         body = driver.find_element(By.CSS_SELECTOR,'body')
         body.send_keys(Keys.PAGE_DOWN)
+        try:
+            element = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.ID, "game_description"))
+            )
+        except:
+            driver.quit()
+
         driver.find_element(By.ID, "fast_adjudication").click()
         driver.find_element(By.ID, "grace_period").click()
     else:
         driver.find_element(By.ID, "begin_when_full").click()
         driver.find_element(By.ID, "skip").click()
+        body = driver.find_element(By.CSS_SELECTOR,'body')
         body.send_keys(Keys.PAGE_DOWN)
         driver.find_element(By.ID, "grace_period").click()
 
